8fb7b30b4bd5ddc5548cbb6472f1261ecf137a7b
Bry <bryanandrewb@gmail.com>
2018-12-05 21:38:56 +0800

    hello world

--- src/main/java/SampleMVN/App.java
+++ src/main/java/SampleMVN/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!!" );
         System.out.println( "This is a PoC demo for our Fortify + SSC integration!" );
         System.out.println( "This sample lines of code will be picked up as a vulnerability" );
         System.out.println( "Have a nice day" );
